ae2c6d2fe1b20290f94e019e3b34f99135092c58
jon <manojam79@gmail.com>
2018-02-08 16:43:54 +1100

    Added field annotations

--- src/com/mmks/dto/HibernateTest.java
+++ src/com/mmks/dto/HibernateTest.java
@@ -10,14 +10,17 @@ public class HibernateTest {
 		UserDetails userDetails = new UserDetails();
 		userDetails.setUserId(1);
 		userDetails.setUserName("first user");
-		
-		SessionFactory sessionFactory  =  new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
-		
+
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+
 		Session session = sessionFactory.openSession();
-		
+
 		session.beginTransaction();
 		session.save(userDetails);
 		session.getTransaction().commit();
+
+		session.close();
+		sessionFactory.close();
 	}
 
 }
--- src/com/mmks/dto/UserDetails.java
+++ src/com/mmks/dto/UserDetails.java
@@ -1,15 +1,22 @@
 package com.mmks.dto;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Transient;
 
-@Entity
+@Entity(name = "USER_DETAILS")
 public class UserDetails {
-	
+
 	@Id
+	@Column(name = "USER_ID")
 	private int userId;
-	
+
+	@Column(name = "USER_NAME")
 	private String userName;
+
+	@Transient
+	private String description;
 
 	public int getUserId() {
 		return userId;
@@ -26,4 +33,13 @@ public class UserDetails {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 }
