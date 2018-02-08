b2dc1f067d410951855400e5a4398f7baa500993
jon <manojam79@gmail.com>
2018-02-08 16:10:11 +1100

    Hibernate 3 Initial commmit

---
+++ src/com/mmks/dto/HibernateTest.java
@@ -0,0 +1,23 @@
+package com.mmks.dto;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateTest {
+
+	public static void main(String[] args) {
+		UserDetails userDetails = new UserDetails();
+		userDetails.setUserId(1);
+		userDetails.setUserName("first user");
+		
+		SessionFactory sessionFactory  =  new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		
+		session.beginTransaction();
+		session.save(userDetails);
+		session.getTransaction().commit();
+	}
+
+}
